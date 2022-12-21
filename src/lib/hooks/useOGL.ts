f40b439ecb141ea8d7390d4b9e664593a5fff53a
KekOnTheWorld <kekontheworld@gmail.com>
2022-12-21 11:28:04 +0100

    Rename to useOGL

--- src/lib/hooks/useOGL.ts
+++ src/lib/hooks/useOGL.ts
@@ -1,6 +1,6 @@
 import { getContext } from "svelte";
 import type { OGLContext } from "../types";
 
-export const useThrelte = (): OGLContext => {
+export const useOGL = (): OGLContext => {
 	return getContext<OGLContext>("svelte-ogl");
 };
